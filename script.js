18beaa9a0c595cfa6dc714c81c98949284106597
subham.singh <subham.singh@zohocorp.com>
2024-04-12 18:53:27 +0530

    temporary launch page

---
+++ script.js
@@ -0,0 +1,23 @@
+var countDownDate = new Date("Jun 01, 2024 00:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days + "d ";
+    document.getElementById("hours").innerHTML = hours + "h ";
+    document.getElementById("minutes").innerHTML = minutes + "m ";
+    document.getElementById("seconds").innerHTML = seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
+document.getElementById("subscribe-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("Form submission prevented");
+});
